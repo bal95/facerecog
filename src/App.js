c89e175c69e332cf2bdb15626cc8f25ff964b7ec
Baljeet Singh <bljitsingh.singh@gmail.com>
2022-06-01 08:33:50 +0530

    included Face Recognition component

--- src/App.js
+++ src/App.js
@@ -4,8 +4,43 @@ import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 import Rank from './components/Rank/Rank'
+import FaceRecognition from './components/FaceRecognition/FaceRecognition'
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
+
+const USER_ID = 'bal95';
+// Your PAT (Personal Access Token) can be found in the portal under Authentication
+const PAT = 'ddc34060d7024141a7b146aea29d23e0';
+const APP_ID = 'f72258ad3e344ef488530c0764ded98b';
+const MODEL_ID = 'face-detection';
+const MODEL_VERSION_ID = '45fb9a671625463fa646c3523a3087d5';
+// Change this to whatever image URL you want to process
+const IMAGE_URL = 'https://samples.clarifai.com/metro-north.jpg';
+
+const raw = JSON.stringify({
+  "user_app_id": {
+      "user_id": USER_ID,
+      "app_id": APP_ID
+  },
+  "inputs": [
+      {
+          "data": {
+              "image": {
+                  "url": IMAGE_URL
+              }
+          }
+      }
+  ]
+});
+
+const requestOptions = {
+  method: 'POST',
+  headers: {
+      'Accept': 'application/json',
+      'Authorization': 'Key ' + PAT
+  },
+  body: raw
+};
 
 const particlesInit = async (main) => {
   console.log(main);
@@ -85,6 +120,24 @@ const particlesOptions={
 }
 
 class App extends Component{
+  constructor(){
+    super()
+    this.state={
+      input:'',
+      imageUrl:'',
+    }
+  }
+  onInputChange=(event)=>{
+    this.setState({input:event.target.value})
+  }
+
+  onSubmit=()=>{
+    this.setState({imageUrl:this.state.input})
+    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
+      .then(response => response.json())
+      .then(result => console.log(result.outputs[0].data.regions[0].region_info.bounding_box))
+      .catch(error => console.log('error', error));
+  }
   render(){
     return(
       <div className="App">
@@ -92,7 +145,8 @@ class App extends Component{
         <Navigation/>
         <Logo/>
         <Rank/>
-        <ImageLinkForm/>
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onSubmit}/>
+        <FaceRecognition imageUrl={this.state.imageUrl}/>
       </div>
     )
   }
